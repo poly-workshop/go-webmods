054907d654c297d2adeffe21dc9602787731faff
KeepingRunning <1599949878@qq.com>
2025-07-01 23:11:48 +0800

    fix: change RequestIDkey to private

--- grpc_utils/interceptor/request_id.go
+++ grpc_utils/interceptor/request_id.go
@@ -9,16 +9,24 @@ import (
 	"google.golang.org/grpc"
 )
 
-type requestIdContextKey string
+type requestIDContextKey string
 
 const (
-	RequestIdKey requestIdContextKey = "request_id"
+	requestIDKey requestIDContextKey = "request_id"
 )
 
-// RequestIdInterceptor is a unary server interceptor that adds a request ID to the context and logs
-func RequestIdInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
-	requestId := uuid.New().String()
-	ctx = context.WithValue(ctx, RequestIdKey, requestId)
-	ctx = app.WithLogAttrs(ctx, slog.String("request_id", requestId))
+// RequestIDInterceptor is a unary server interceptor that adds a request ID to the context and logs
+func RequestIDInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+	requestID := uuid.New().String()
+	ctx = context.WithValue(ctx, requestIDKey, requestID)
+	ctx = app.WithLogAttrs(ctx, slog.String("request_id", requestID))
 	return handler(ctx, req)
+}
+
+// GetRequestID extracts the request ID from the context
+func GetRequestID(ctx context.Context) string {
+	if requestID, ok := ctx.Value(requestIDKey).(string); ok {
+		return requestID
+	}
+	return ""
 }
