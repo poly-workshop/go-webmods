840e2f1ef3b499c5487353e653ab6bbde9ee90bc
KeepingRunning <1599949878@qq.com>
2025-07-02 10:20:24 +0800

    fix: remove requestIDContextKey and uuid in context

--- grpc_utils/interceptor/request_id.go
+++ grpc_utils/interceptor/request_id.go
@@ -9,24 +9,9 @@ import (
 	"google.golang.org/grpc"
 )
 
-type requestIDContextKey string
-
-const (
-	requestIDKey requestIDContextKey = "request_id"
-)
-
 // RequestIDInterceptor is a unary server interceptor that adds a request ID to the context and logs
 func RequestIDInterceptor(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 	requestID := uuid.New().String()
-	ctx = context.WithValue(ctx, requestIDKey, requestID)
 	ctx = app.WithLogAttrs(ctx, slog.String("request_id", requestID))
 	return handler(ctx, req)
-}
-
-// GetRequestID extracts the request ID from the context
-func GetRequestID(ctx context.Context) string {
-	if requestID, ok := ctx.Value(requestIDKey).(string); ok {
-		return requestID
-	}
-	return ""
 }
